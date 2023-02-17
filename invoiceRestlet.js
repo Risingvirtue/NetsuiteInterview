2588c34226c3367034d4cf8211fd13fb9ef79616
Johnny On <risingvirtue@berkeley.edu>
2023-02-17 10:29:15 -0800

    search and return result if there is one invoice

--- invoiceRestlet.js
+++ invoiceRestlet.js
@@ -5,14 +5,41 @@
 define(['N/record', 'N/task'],
     function(record, task) {
        
-        // Get a standard NetSuite record
-        function _get(context) {
-    
-            return JSON.stringify({test: 'test get'});
+        // Get an invoice
+        function get(context) {
+			var invoiceNumber = context.request.parameters.invoicenumber;
+			//validate invoice number
+			if (!invoiceNumber) {
+				return "No invoice number provided.";
+			}
+			//create a saved search for invoice
+			var columns = [{name: 'internalid'},
+							{name: 'tranid'}, //invoice number
+							{name: 'entity'}, //customer
+							{name: 'subsidiary'},
+							{name: 'total'}];
+			var filters = [{name: 'tranid', operator: 'anyof', values: [location]}];
+			var searchResult = search.create({
+				type: 'invoice',
+				columns: columns,
+				filters: filters
+			})
+			var results = searchResult.run().getRange({ start: 0, end: 2 });
+			if (results.length == 0) {
+				return "No invoice found.";
+			} else if (results.length == 1) {
+				return {
+					'internalid': results[0].getValue({name: 'internalid'}),
+					'invoicenumber': results[0].getValue({name: 'tranid'}),
+					'customer': results[0].getValue({name: 'entity'}),
+					'subsidiary': results[0].getValue({name: 'subsidiary'}),
+					'totalamount': results[0].getValue({name: 'total'})
+				}
+			} else {
+				return "Multiple invoices found.";
+			}
         }
         
- 
-	
         return {
             get: get
         };
